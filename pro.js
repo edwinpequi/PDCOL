02f7886afb50fde4fd92c7fe187d7a5496ed7af9
edwin martinez <edwimarti@gmail.com>
2018-04-30 17:56:21 -0500

    nuevos archivos del protecto

---
+++ pro.js
@@ -0,0 +1,18 @@
+alert('hola PDCOL');
+
+var nombre = 'APRENDIZ';
+var edad = 5;
+
+alert(nombre);
+
+console.log('hola consola');
+
+function saludo(){//  creamos una funcion
+
+alert(nombre);
+alert(edad);
+
+}
+
+
+saludo();//llamando las funciones
